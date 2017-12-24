35d827ce399ed1e19af73d38734984a7f5b00c85
NunoGarcia <NunoGarcia@DESKTOP-RLMITTG.home>
2017-12-24 20:28:38 +0000

    Painel codificado.
    Falta dar uns ajustes nos utilizadores.

--- src/view/Intro.java
+++ src/view/Intro.java
@@ -1,5 +1,6 @@
 package view;
 
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Toolkit;
@@ -17,13 +18,17 @@ import javax.swing.border.EmptyBorder;
 
 import controller.AppCtrl;
 import model.User;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class Intro extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JPanel panel;
 	private AppCtrl app;
 	private JTextField textField;
+	boolean isAlreadyEnable = false;
 	
 	
 	public Intro() {
@@ -34,14 +39,14 @@ public class Intro extends JFrame {
 		this.app = new AppCtrl();
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 405, 330);
+		setBounds(100, 100, 405, 375);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JPanel panel = new JPanel();
-		panel.setBounds(10, 11, 375, 278);
+		panel = new JPanel();
+		panel.setBounds(10, 11, 375, 324);
 		contentPane.add(panel);
 		panel.setLayout(null);
 		
@@ -61,33 +66,7 @@ public class Intro extends JFrame {
 		btnEntrar.setEnabled(false);
 		btnEntrar.setBounds(205, 159, 154, 101);
 		panel.add(btnEntrar);
-		
-		
-		JButton button_5 = new JButton("5");
-		button_5.setEnabled(false);
-		button_5.setBounds(75, 159, 55, 45);
-		panel.add(button_5);
-		
-		JButton button_6 = new JButton("6");
-		button_6.setEnabled(false);
-		button_6.setBounds(140, 159, 55, 45);
-		panel.add(button_6);
-		
-		JButton button_7 = new JButton("7");
-		button_7.setEnabled(false);
-		button_7.setBounds(10, 215, 55, 45);
-		panel.add(button_7);
-		
-		JButton button_8 = new JButton("8");
-		button_8.setEnabled(false);
-		button_8.setBounds(75, 215, 55, 45);
-		panel.add(button_8);
-		
-		JButton button_9 = new JButton("9");
-		button_9.setEnabled(false);
-		button_9.setBounds(140, 215, 55, 45);
-		panel.add(button_9);
-		
+
 		textField = new JTextField();
 		textField.setEnabled(false);
 		textField.setEditable(false);
@@ -99,18 +78,30 @@ public class Intro extends JFrame {
 		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
 		this.setLocation(dim.width/2-this.getSize().width/2, dim.height/2-this.getSize().height/2);
 		
+		
+		for (JButton btn : this.createBtns())
+		{
+			panel.add(btn);
+		}
+		
 		comboBox.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
-				JComboBox cb = (JComboBox) e.getSource();
+				@SuppressWarnings("unchecked")
+				JComboBox<String> cb = (JComboBox<String>) e.getSource();
 				Object s = cb.getSelectedItem();
 				
-				for(User u : Intro.this.getApp().arUsers.getArUsers())
-				{
-					if (u.getUsername().equals(s.toString()))
-					{
-						
+				if(cb.getSelectedIndex() == 0) {
+					// bloqueia o teclado
+					lockBtns();
+				}
+				else {
+					for(User u : Intro.this.getApp().arUsers.getArUsers()) {
+						// desbloqueia o teclado
+						if (u.getUsername().equals(s.toString())) {
+							unlockBtns();
+						}
 					}
 				}
 			}
@@ -121,15 +112,33 @@ public class Intro extends JFrame {
 	// preenche o menu dropdwon com o nome dos utilizadores
 	private JComboBox<String> addItemComboBox(ArrayList<User> au) {
 		JComboBox<String> jc = new JComboBox<>();
+		// isAlreadyEnable está decladara globalmente
+		jc.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if(e.isControlDown())
+				{
+					if(e.getKeyCode() == 75)
+					{
+						if(isAlreadyEnable) {
+							lockBtns();
+							isAlreadyEnable = false;
+						}
+						else {
+							unlockBtns();
+							isAlreadyEnable = true;
+						}
+					}
+				}
+			}
+		});
 		jc.addItem("--- Selecione ---");
-		for(User u : au)
-		{
+		for(User u : au) {
 			if(u.getId() != 0)
 			{
 				jc.addItem(u.getUsername());
 			}
 		}
-		
 		return jc;
 	}
 
@@ -138,45 +147,72 @@ public class Intro extends JFrame {
 		return app;
 	}
 	
-	// Função para criar o painel de botões
-	
+	// Função para criar o painel de botoes
 	private ArrayList<JButton> createBtns() {
 		
+		Font btn_font = new Font("Arial Black", Font.BOLD, 16);
 		ArrayList<JButton> arBtn = new ArrayList<JButton>();
 		
-		JButton button_1 = new JButton("1");
-		button_1.setEnabled(false);
-		button_1.setBounds(10, 103, 55, 45);
-		panel.add(button_1);
+		int btn_w = 55;
+		int btn_h = 45;
+		int btn_pos_x = 10;
+		int btn_pos_y = 103;
+		int aux[][] = { {btn_pos_x, btn_pos_y}, {btn_pos_x, btn_pos_y+56}, {btn_pos_x, btn_pos_y+112} };
 		
-
-		JButton button_2 = new JButton("2");
-		button_2.setEnabled(false);
-		button_2.setBounds(75, 103, 55, 45);
-		panel.add(button_2);
-		
-		JButton button_3 = new JButton("3");
-		button_3.setEnabled(false);
-		button_3.setBounds(140, 103, 55, 45);
-		panel.add(button_3);
-		
-		JButton button_4 = new JButton("4");
-		button_4.setEnabled(false);
-		button_4.setBounds(10, 159, 55, 45);
-		panel.add(button_4);
-		
-		
-		int ct=0;
-		for(int i=0; i <= 9 ; i++) {
-			
-			JButton btn = new JButton(""+i);
+		for(int ct=0; ct < 11; ct++)
+		{
+			JButton btn = new JButton(""+ct);
+			btn.setFont(btn_font);
 			btn.setEnabled(false);
-			btn.setBounds(10, 103, 55, 45);
+			
+			if(ct == 0)
+			{
+				btn.setBounds(75, 271, 120, 45);
+			}
+			else if(ct > 0 && ct < 4)
+			{
+				btn.setBounds(aux[0][0], aux[0][1], btn_w, btn_h);
+				aux[0][0] += btn_pos_x+btn_w;
+			}
+			else if(ct > 3 && ct < 7)
+			{
+				btn.setBounds(aux[1][0], aux[1][1], btn_w, btn_h);
+				aux[1][0] += btn_pos_x+btn_w;
+			}
+			else if(ct > 6 && ct <= 9)
+			{
+				btn.setBounds(aux[2][0], aux[2][1], btn_w, btn_h);
+				aux[2][0] += btn_pos_x+btn_w;
+			}
+			else
+			{
+				btn.setText("C");
+				btn.setBounds(btn_pos_x, 271, btn_w, btn_h);
+			}
+			arBtn.add(btn);
+		}
+		return arBtn;
+	}
+	
+	// Desbloqueia os botoes
+	private void unlockBtns() {
+		Component[]comp = this.panel.getComponents();
+		for(int i=0; i < comp.length; i++) {
+			if(comp[i] instanceof JButton) {
+				JButton btn = (JButton) comp[i];
+				btn.setEnabled(true);
+			}
 		}
 	}
 	
-	// Desbloqueia os botões
-	private void unlockBtns() {
-		
+	// Bloqueia os botoes
+	private void lockBtns() {
+		Component[]comp = this.panel.getComponents();
+		for(int i=0; i < comp.length; i++) {
+			if(comp[i] instanceof JButton) {
+				JButton btn = (JButton) comp[i];
+				btn.setEnabled(false);
+			}
+		}
 	}
 }
